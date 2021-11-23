a72cb31429ac5ca4600b3ec427e94f18f2d4797c
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-23 23:03:48 +0530

    Update TestLabThread.java

--- src/TestLabThread.java
+++ src/TestLabThread.java
@@ -2,6 +2,7 @@
 //}
 class Mythread extends Thread
 {
+
     public static void main(String[] args) {
         Thread obj =Thread.currentThread();
         obj.setName("Yash"); // we can change the name of the thread method by set name method
