4294f459970235245b8e623e628244f676e8a003
Yash Gupta <yash8917gupta@gmail.com>
2021-12-04 16:58:31 +0530

    Overloading of run Method

--- src/MultithreadingExa_2.java
+++ src/MultithreadingExa_2.java
@@ -1,7 +1,20 @@
 class Mythrad2 extends Thread{
-    public void run(){
 
+    public void run(){
+        System.out.println("run Method");
+    }
+    //Overloaded Method called by Explecitly
+    public void run(int i){
+        System.out.println("Overloaded Method");
     }
 }
 public class MultithreadingExa_2 {
+    public static void main(String[] args) {
+        Mythread2 t=new Mythread2();
+        t.start();
+        System.out.println("Main Method");
+        // o
+        //o/p is -run Method
+        //run method overloading is possible
+    }
 }
